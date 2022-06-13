7693d12a2dc43e42832a868ffe81f7d320348fea
dojo2022-saburi <103239318+dojo2022-saburi@users.noreply.github.com>
2022-06-13 14:01:17 +0900

    佐分担当分model・javascript・css

---
+++ src/src/model/BodyImage.java
@@ -0,0 +1,14 @@
+package model;
+
+public class BodyImage {
+
+	public BodyImage() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ src/src/model/Graduate.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Graduate {
+
+	public Graduate() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
