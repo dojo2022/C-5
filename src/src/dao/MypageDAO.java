f8967c3ee21c4a7ca7e4b994e289dd058baaa65c
dojo2022-kanezashi <103225886+dojo2022-kanezashi@users.noreply.github.com>
2022-06-15 10:43:44 +0900

    Update MypageDAO.java

--- src/src/dao/MypageDAO.java
+++ src/src/dao/MypageDAO.java
@@ -1,23 +1,13 @@
 package dao;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import model.Mypage;
-public class MypageDAO {
-	public boolean makeIllustration(Mypage ){
-		try {
-
-			Connection conn = null;
+//	Connection conn = null;
 			// JDBCドライバを読み込む
-			Class.forName("org.h2.Driver");
+		//	Class.forName("org.h2.Driver");
 			// データベースに接続する
-						conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/C5", "sa", "");
+		//				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/C5", "sa", "");
 
-		
-		}
-	}
 
-}
+		//}
+//	}
+
+//}
