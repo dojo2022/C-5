e5998aa5e9bd3893642105008d9695892b4c5211
koji higashi <k-higashi@3plus.co.jp>
2022-06-16 08:14:54 +0900

    ２つのタブ切替対応
    対応方法は以下のとおり、２つめのタブのis-activeとis-showクラス名を１つめのタブと変更
    ※jspとcssとjsファイルに変更あります。
    jsファイルののイベント割り付けですが、無名関数を使わないと読み込まれないんので関数名を無名関数にしています。
    
    しっかりとスーパーりーろーどしてからご確認ください！

--- src/WebContent/WEB-INF/jsp/Mypage.jsp
+++ src/WebContent/WEB-INF/jsp/Mypage.jsp
@@ -24,62 +24,66 @@
 
 </header>
 <body>
-<!--  <div class="hamburger">
+	<!--  <div class="hamburger">
   <span></span>
   <span></span>
   <span></span>
 </div>-->
-<hr>
+	<hr>
 
 
-<!-- アバター表示用タブ---------------------------------- -->
-<div class="avater_photo">
-  <!--タブ-->
+	<!-- アバター表示用タブ---------------------------------- -->
+	<div class="avater_photo">
+		<!--タブ-->
 
-    <ul class="avater-group">
-    	<li class="avater avater-now is-active">現在</li>
-    	<li class="avater avater-ideal">理想</li>
- 	</ul>
+		<ul class="avater-group">
+			<li class="avater avater-now is-active-avater">現在</li>
+			<li class="avater avater-ideal">理想</li>
+		</ul>
 
-  <!--タブを切り替えて表示するコンテンツ-->
-  <div class="photo-group">
-    <div class="photo avater-now is-show">現在</div>
-    <div class="photo avater-ideal">理想</div>
-  </div>
-</div>
-
-
-<!-- 目標達成報告フォーム -------------------------------------------- -->
-
-<form method="POST" action="/health_management/ShortChecklistServlet" id="report">
-<!-- <form method="POST" action="/health_management/ResultServlet" id="report"> -->
-<div class=first>
-		長期目標　○○　<br>
-		短期目標　○○　　<input type="checkbox" name="short" value="check"><br><br></div>
-		体重<input type="text" name="height" value="">kg　
-<input type="submit" name="report" value="報告"><br><br>
-</form>
+		<!--タブを切り替えて表示するコンテンツ-->
+		<div class="photo-group">
+			<div class="photo avater-now is-show-avater">現在</div>
+			<div class="photo avater-ideal">理想</div>
+		</div>
+	</div>
 
 
-<!-- 達成経過表示用タブ ---------------------------------------------- -->
-<div class="tab-panel">
-  <!--タブ-->
+	<!-- 目標達成報告フォーム -------------------------------------------- -->
 
-  <ul class="tab-group">
-    <li class="tab tab-A is-active">達成状況</li>
-    <li class="tab tab-B">体重推移</li>
+	<form method="POST" action="/health_management/ShortChecklistServlet"
+		id="report">
+		<!-- <form method="POST" action="/health_management/ResultServlet" id="report"> -->
+		<div class=first>
+			長期目標 ○○ <br> 短期目標 ○○ <input type="checkbox" name="short"
+				value="check"><br>
+			<br>
+		</div>
+		体重<input type="text" name="height" value="">kg <input
+			type="submit" name="report" value="報告"><br>
+		<br>
+	</form>
 
- </ul>
 
-  <!--タブを切り替えて表示するコンテンツ-->
-  <div class="panel-group">
-    <div class="panel tab-A is-show">●●●●●●●●●●</div>
-    <div class="panel tab-B">○○○○○○○○○○○○</div>
-  </div>
-</div>
-<!-- ---------------------------------------------- -->
+	<!-- 達成経過表示用タブ ---------------------------------------------- -->
+	<div class="tab-panel">
+		<!--タブ-->
 
-<!-- -------------------------------------------- -->
+		<ul class="tab-group">
+			<li class="tab tab-A is-active">達成状況</li>
+			<li class="tab tab-B">体重推移</li>
+
+		</ul>
+
+		<!--タブを切り替えて表示するコンテンツ-->
+		<div class="panel-group">
+			<div class="panel tab-A is-show">●●●●●●●●●●</div>
+			<div class="panel tab-B">○○○○○○○○○○○○</div>
+		</div>
+	</div>
+	<!-- ---------------------------------------------- -->
+
+	<!-- -------------------------------------------- -->
 </body>
 <script src="/health_management/js/mypage.js"></script>
 </html>
--- src/WebContent/js/mypage.js
+++ src/WebContent/js/mypage.js
@@ -1,43 +1,43 @@
 
-document.addEventListener('DOMContentLoaded', function(){
-  // タブに対してクリックイベントを適用
-  const tabs = document.getElementsByClassName('tab');
-  for(let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', tabSwitch, false);
-  }
+document.addEventListener('DOMContentLoaded', function() {
+	// タブに対してクリックイベントを適用
+	const tabs = document.getElementsByClassName('tab');
+	for (let i = 0; i < tabs.length; i++) {
+		tabs[i].addEventListener('click', tabSwitch, false);
+	}
 
-  // タブをクリックすると実行する関数
-  function tabSwitch(){
-    // タブのclassの値を変更
-    document.getElementsByClassName('is-active')[0].classList.remove('is-active');
-    this.classList.add('is-active');
-    // コンテンツのclassの値を変更
-    document.getElementsByClassName('is-show')[0].classList.remove('is-show');
-    const arrayTabs = Array.prototype.slice.call(tabs);
-    const index = arrayTabs.indexOf(this);
-    document.getElementsByClassName('panel')[index].classList.add('is-show');
-  };
+	// タブをクリックすると実行する関数
+	function tabSwitch() {
+		// タブのclassの値を変更
+		document.getElementsByClassName('is-active')[0].classList.remove('is-active');
+		this.classList.add('is-active');
+		// コンテンツのclassの値を変更
+		document.getElementsByClassName('is-show')[0].classList.remove('is-show');
+		const arrayTabs = Array.prototype.slice.call(tabs);
+		const index = arrayTabs.indexOf(this);
+		document.getElementsByClassName('panel')[index].classList.add('is-show');
+	};
 }, false);
 
 /*-----------------------------------------------------------------------------*/
 
 
-document.addEventListener('DOMContentLoaded', function avater(){
-  // タブに対してクリックイベントを適用
-  const avaters = document.getElementsByClassName('avater');
-  for(let j = 0; j < avaters.length; j++) {
-    avaters[j].addEventListener('click', avaterSwitch, false);
-  }
+document.addEventListener('DOMContentLoaded', function() {
+	// タブに対してクリックイベントを適用
+	const avaters = document.getElementsByClassName('avater');
+	for (let j = 0; j < avaters.length; j++) {
+		avaters[j].addEventListener('click', avaterSwitch, false);
+	}
 
-  // タブをクリックすると実行する関数
-  function avaterSwitch(){
-    // タブのclassの値を変更
-    document.getElementsByClassName('is-active')[0].classList.remove('is-active');
-    this.classList.add('is-active');
-    // コンテンツのclassの値を変更
-    document.getElementsByClassName('is-show')[0].classList.remove('is-show');
-    const arrayAvaters = Array.prototype.slice.call(avaters);
-    const index = arrayAvaters.indexOf(this);
-    document.getElementsByClassName('photo')[index].classList.add('is-show');
-  };
+	// タブをクリックすると実行する関数
+	function avaterSwitch() {
+		// タブのclassの値を変更
+		document.getElementsByClassName('is-active-avater')[0].classList.remove('is-active-avater');
+		this.classList.add('is-active-avater');
+		// コンテンツのclassの値を変更
+		document.getElementsByClassName('is-show')[0].classList.remove('is-show-avater');
+		const arrayAvaters = Array.prototype.slice.call(avaters);
+		const index = arrayAvaters.indexOf(this);
+		document.getElementsByClassName('photo')[index].classList.add('is-show-avater');
+	};
 }, false);
