618795c85ba47d6beadc04ff8eb1e13070117e6b
dojo2022-kanezashi <103225886+dojo2022-kanezashi@users.noreply.github.com>
2022-06-21 14:51:01 +0900

    anima

--- src/WebContent/js/animation.js
+++ src/WebContent/js/animation.js
@@ -2,12 +2,12 @@
   var myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ['8月1日', '8月2日', '8月3日', '8月4日', '8月5日', '8月6日', '8月7日'],
+      labels: ['', '', '', '', '', '', '','', '', '', '', '', '', ''],
       //foreach使えば行けるのでは
       datasets: [
         {
           label: '体重推移',
-          data: [60.2, 60.4, 59.8, 60.1, 58.7, 58.0, 57.1, 56.4],
+          weight: [ , , , , , , , , , , , , , ],
           //ここもforeach
           borderColor: "rgba(255,0,0,1)",
           backgroundColor: "rgba(0,0,0,0)"
@@ -27,3 +27,9 @@
       },
     }
   });
+  for(h=0;h<   .length;h++){
+  for(i=0;i<14;i++){
+  labels[i]=
+
+  }
+  }
