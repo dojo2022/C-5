b0aaf2f0696bd352329808c9cb5979dc021329bc
dojo2022-kanezashi <103225886+dojo2022-kanezashi@users.noreply.github.com>
2022-06-22 11:56:50 +0900

    Update AnimationServlet.java

--- src/src/servlet/AnimationServlet.java
+++ src/src/servlet/AnimationServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.AnimationDAO;
 import model.Mypage;
@@ -31,15 +32,19 @@ public class AnimationServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*HttpSession session = request.getSession();
-		if (session.getAttribute("user_id") == null) {
+		HttpSession session = request.getSession();
+		/*if (session.getAttribute("user_id") == null) {
 			response.sendRedirect("/health_management/LoginServlet");
 			return;
 		}*/
 
+
+
 		AnimationDAO aDao = new AnimationDAO();
-		List<Mypage> cardList = aDao.select(new Mypage());
-		request.setAttribute("cardList", cardList);
+		List<Mypage> animationList = aDao.select(new Mypage(null,0));
+
+
+		request.setAttribute("animationList", animationList);
 		// 体重推移アニメーションページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Animation.jsp");
 				dispatcher.forward(request, response);
