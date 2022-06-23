ea49418065abc6189415f3977b99057033efbfb9
dojo2022-kanezashi <103225886+dojo2022-kanezashi@users.noreply.github.com>
2022-06-23 17:13:40 +0900

    qqq

--- src/src/servlet/ResultServlet.java
+++ src/src/servlet/ResultServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.AnimationDAO;
+import model.Mypage;
 
 /**
  * Servlet implementation class ResultServlet
@@ -20,6 +24,14 @@ public class ResultServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String animation_id ="id_kanehira"; //(String)session.getAttribute("user_id");//
+//		リクエストスコープにuser_idを入れる 疑似的にuser_id入れてるので悪しからず
+		request.setAttribute("user_id", animation_id);
+
+		AnimationDAO aDao = new AnimationDAO();
+		List<Mypage> animationList = aDao.select(animation_id);
+
+		request.setAttribute("animationList", animationList);
 		// 達成評価ページにフォワードする
 				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Result.jsp");
 				dispatcher.forward(request, response);
